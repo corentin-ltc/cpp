8352535070ec2e857fe1746038d886f491afc5e1
Corentin Le Tortorec <cle-tort@paul-f4Ar8s6.clusters.42paris.fr>
2024-11-16 01:26:42 +0100

    fixed 04 and save for 05

--- 01/ex04/srcs/utils.cpp
+++ 01/ex04/srcs/utils.cpp
@@ -2,7 +2,15 @@
 
 void copy_in_outfile(char **argv, std::string content)
 {
-	std::ofstream outfile(((std::string)argv[1]).append(".replace"));
+	std::string outfile_name;
+
+	outfile_name = ((std::string)argv[1]).append(".replace");
+	std::ofstream outfile(outfile_name);
+	if (!outfile)
+	{
+		std::cerr << "Could not open \"" << outfile_name <<"\"." << std::endl; 
+		exit(EXIT_FAILURE);
+	}
 	outfile << content;
 	outfile.close();
 }
@@ -46,7 +54,7 @@ void check_arguments(int argc, char **argv)
 {
 	if (argc != 4)
 		exit_error("Wrong number of arguments.");
-	if (!argv[1])
+	if (!argv[1][0])
 		exit_error("The file name must not be null.");
 }
 
