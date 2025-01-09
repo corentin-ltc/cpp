4cc88a3e12b6a1b1ae97d662d7eac700695878c0
Corentin Le Tortorec <cle-tort@bess-f2r2s7.clusters.42paris.fr>
2025-01-09 23:46:57 +0100

    CPP08 done

--- 08/ex02/includes/MutantStack.hpp
+++ 08/ex02/includes/MutantStack.hpp
@@ -8,7 +8,7 @@
 
 
 template <typename T>
-class MutantStack : public std::stack
+class MutantStack : public std::stack<T>
 {
     public:
 
@@ -19,6 +19,19 @@ class MutantStack : public std::stack
 		~MutantStack();
 		MutantStack &operator=(const MutantStack &other);      
 
+    typedef typename std::deque<T>::iterator iterator;
+    typedef typename std::deque<T>::const_iterator const_iterator;
+    typedef typename std::deque<T>::reverse_iterator reverse_iterator;
+    typedef typename std::deque<T>::const_reverse_iterator const_reverse_iterator;
+
+    iterator begin() { return c.begin(); }
+    iterator end() { return c.end(); }
+    const_iterator begin() const { return c.begin(); }
+    const_iterator end() const { return c.end(); }
+    reverse_iterator rbegin() { return c.rbegin(); }
+    reverse_iterator rend() { return c.rend(); }
+    const_reverse_iterator rbegin() const { return c.rbegin(); }
+    const_reverse_iterator rend() const { return c.rend(); }
 };
 
 template <class T>
